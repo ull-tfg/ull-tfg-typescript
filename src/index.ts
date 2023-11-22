b239b051853949796009efcb7e91af3c9bb73adb
cexposit <cexposit@ull.edu.es>
2023-11-22 22:38:35 +0000

    mc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { ullFormRules } from './ull-form-rules';
+export { UllFormRules } from './ull-form-rules';
 export { http } from './ull-http';
 export { Either } from './ull-either';
 export type { DataError, ApiError, UnexpectedError, Unauthorized, NotFound } from './ull-data-error';
 export { simplify } from './ull-string';
 export { UllUUID as KaiztenUUID } from './ull-uuid';
-export { KaiztenHttpResponseStatusCode } from './ull-http-response-status-code';
+export { UllHttpResponseStatusCode } from './ull-http-response-status-code';
 export { kaiztenDate } from './ull-date';
